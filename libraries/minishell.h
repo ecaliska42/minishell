5bddfaf079f1168c0a1eb766a3ef4e46357c304c
Melih Senyurt <mesenyur@c2r3p9.42vienna.com>
2024-04-11 12:14:31 +0200

    upd

--- libraries/minishell.h
+++ libraries/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ecaliska <ecaliska@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mesenyur <mesenyur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 15:22:56 by ecaliska          #+#    #+#             */
-/*   Updated: 2024/04/10 17:20:51 by ecaliska         ###   ########.fr       */
+/*   Updated: 2024/04/10 17:28:21 by mesenyur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -141,8 +141,8 @@ int	t_env_size(t_env **envp);
 int	array_size(char **array);
 
 
-void 	expansion(t_token *token, t_env *envp, char quotes);
-t_token	*expand_variable(t_token *token, t_env *envp, char quotes);//, int flag);
+void 	expansion(t_token *token, t_env *envp);
+t_token	*expand_variable(t_token *token, t_env *envp, char quotes);
 char	*get_env_value(char *name, t_env *envp);
 char 	*add_char(char *str, char new_char);
 t_env	*get_from_env(t_env **lst, char *s);
