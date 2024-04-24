9dcf87eb5a1073c440b24595445f1dee8d88692e
Melih Senyurt <mesenyur@c3r5p5.42vienna.com>
2024-04-24 15:23:54 +0200

    updated exit

--- libft/utilities/ft_atol.c
+++ libft/utilities/ft_atol.c
@@ -6,24 +6,31 @@
 /*   By: mesenyur <mesenyur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/24 17:27:15 by ecaliska          #+#    #+#             */
-/*   Updated: 2024/04/18 16:41:02 by mesenyur         ###   ########.fr       */
+/*   Updated: 2024/04/24 15:22:13 by mesenyur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 #include <limits.h>
 
+int check_if_valid(long number, int digit, int sign)
+{
+    if (sign > 0 && (number > LONG_MAX / 10 || (number == LONG_MAX / 10 && digit > 7)))
+        return (false);
+    if (sign < 0 && (number < LONG_MIN / 10 || (number == LONG_MIN / 10 && digit > 8)))
+        return (false);
+    return (true);
+}
+
 long	ft_atol(const char *str)
 {
 	long	number;
-	int		sign;
+	long	sign;
 	int		i;
 
 	number = 0;
 	sign = 1;
 	i = 0;
-	while (str[i] && (str[i] >= 9 && str[i] <= 13))
-		i++;
 	if (str[i] == '+' || str[i] == '-')
 	{
 		if (str[i] == '-')
@@ -32,9 +39,11 @@ long	ft_atol(const char *str)
 	}
 	while (str[i] >= '0' && str[i] <= '9')
 	{
-		if (number > LONG_MAX / 10
-			|| (number == LONG_MAX / 10 && str[i] - '0' > 7))
-			printf("numeric numbers required\n");
+		if (!check_if_valid(number, str[i] - '0', sign))
+		{
+			ft_putendl_fd("Numeric argument required", 2);
+    		return (2);
+		}
 		number = (number * 10) + (str[i] - '0');
 		i++;
 	}
